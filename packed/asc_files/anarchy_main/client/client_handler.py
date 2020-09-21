054ccc9157c1e45e7d57083d11606676b4419850
Dscha <Eggi2105@googlemail.com>
2020-09-21 03:50:26 +0200

    Backend: client_handler: Setting of Standard gear (Dev-Gear), when player connects for the first time (new to Database)

--- packed/asc_files/anarchy_main/client/client_handler.py
+++ packed/asc_files/anarchy_main/client/client_handler.py
@@ -24,11 +24,13 @@ def client_add(**kwargs):
 		'inv_grid': inv_handler.invGrid_create(8, 4),
 		'itemData': {},
 		'gear':     {
-			'helmet': {},
+			# 'helmet': item_handler.item_create("vn_c_headband_03"),
+			'helmet': item_handler.item_create("vn_b_bandana_03"),
 			'goggles': {},
-			'backpack': {},
+			'backpack': item_handler.item_create("vn_c_pack_01"),
 			'vest': {},
-			'uniform': {},
+			# 'uniform': item_handler.item_create("vn_o_uniform_vc_01_05"),
+			'uniform': item_handler.item_create("vn_b_uniform_macv_01_06"),
 			'pouch': {},
 			'tool': {},
 			'w_main': {},
@@ -45,6 +47,9 @@ def client_add(**kwargs):
 		}
 
 	cData.update(kwargs)
+	cData["gear"]["helmet"]["curInv"] = cData["puid"]
+	cData["gear"]["uniform"]["curInv"] = cData["puid"]
+	cData["gear"]["backpack"]["curInv"] = cData["puid"]
 	# print(item)
 	return cData
 
@@ -56,6 +61,18 @@ def client_init(self):
 		self.cData = client_add(puid=self.puid)
 		self.sData.database.player_data_set(self.puid, self.cData)
 
+	# Server:
+	# send gear over to the Server, so it can equip the player:
+	print(f"SENDING DATASET FROM {self.puid} TO SERVER...")
+	dataset = {
+		"data_puid": self.puid,
+		"data_gear": self.cData["gear"]
+		}
+	asc_g_msg.sendMsg("loadout_set", dataset, self.sData.con_gameServer)
+	print(f"SENDING DATASET FROM {self.puid} TO SERVER... DONE")
+
+
+	# Client:
 	# send item Data
 	print(f"SENDING INIT_ITEMDATA TO {self.puid}... ")
 	asc_g_msg.sendMsg("INIT_ITEMDATA", self.sData.itemParentData, self.con_client)
