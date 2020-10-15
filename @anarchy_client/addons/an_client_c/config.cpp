2a7e374f08d969019cca6b49adfe08a117e6ebd0
Dscha <Eggi2105@googlemail.com>
2020-10-15 04:25:35 +0200

    Loading of all existing para Rsc defines in Anarchy

--- @anarchy_client/addons/an_client_c/config.cpp
+++ @anarchy_client/addons/an_client_c/config.cpp
@@ -27,3 +27,34 @@ class CfgFunctions
 		#include "global\functions\functions_g.h"
 	};
 };
+
+//// UI
+class para_RscText;
+class para_RscPicture;
+class para_RscPictureKeepAspect;
+class para_RscStructuredText;
+class para_RscStructuredText_c;
+class para_RscStructuredText_r;
+class para_RscListNBox;
+class para_RscListBox;
+class para_RscButton;
+class para_RscButton_ImgSwitch;
+class para_RscControlsGroup;
+class para_RscControlsGroupNoScrollbarHV;
+class para_RscControlsGroupNoScrollbarH;
+class para_RscControlsGroupNoScrollbarV;
+class para_RscCombo;
+class para_RscMapControl;
+class para_RscShortcutButton;
+class para_RscButtonMenu;
+class para_RscEdit;
+class ScrollBar;	// ToDo: Define "para_scrollBar"
+class para_RscCheckBoxes;
+class para_ControlsTable;
+class para_RscProgress;
+class para_RscStatProgress;
+class para_RscStatProgressHUD;
+
+//UI Defines
+#include "\sgd\paradigm\client\configs\ui\ui_def_base.inc"
+
