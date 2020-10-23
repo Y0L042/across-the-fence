788ad460786d19464e679a6a0a79f429c5b14a28
Dscha <Eggi2105@googlemail.com>
2020-10-23 14:18:58 +0200

    Client: _functions_asc: ASC_init removed, DEV_INIT added

--- @anarchy_client/addons/an_client_c/client/functions/asc/_functions_asc.h
+++ @anarchy_client/addons/an_client_c/client/functions/asc/_functions_asc.h
@@ -2,7 +2,7 @@
 
 class ASC
 {
-    DECLARE_CLIENT_FUNC_POSTINIT(asc,asc_init);
+    DECLARE_CLIENT_FUNC_POSTINIT(asc,DEV_init);
     DECLARE_CLIENT_FUNC(asc,clientData_init);
     DECLARE_CLIENT_FUNC(asc,ext_CE_callback_client);
 };
