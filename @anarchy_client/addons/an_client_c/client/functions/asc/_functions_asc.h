2ab66686c766463482161991469ae8ec25ead1ad
Dscha <Eggi2105@googlemail.com>
2020-11-04 18:50:26 +0100

    Client: clientData_init removed from functions

--- @anarchy_client/addons/an_client_c/client/functions/asc/_functions_asc.h
+++ @anarchy_client/addons/an_client_c/client/functions/asc/_functions_asc.h
@@ -3,7 +3,6 @@
 class ASC
 {
     DECLARE_CLIENT_FUNC_POSTINIT(asc,DEV_init);
-    DECLARE_CLIENT_FUNC(asc,clientData_init);
     DECLARE_CLIENT_FUNC(asc,ext_CE_callback_client);
     DECLARE_CLIENT_FUNC(asc,client_connect);
 };
