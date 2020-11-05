150ce1456c12306e71774b0508ac1a2400103288
Dscha <Eggi2105@googlemail.com>
2020-11-05 01:48:14 +0100

    Backend: item_handler: inv_update_force added, when "errors" occur.

--- packed/asc_files/anarchy_main/inventory/item_handler.py
+++ packed/asc_files/anarchy_main/inventory/item_handler.py
@@ -100,24 +100,17 @@ def item_move(client=None, args=()):
     oldInv_invGrid = oldInv["inv_grid"]
     # print(f"::::: OLD INV:\n{oldInv}")
     # print(f"::::: OLD INV GRID:\n{oldInv_invGrid}")
+
     item = None
     # and the item data
     if itemID in oldInv["itemData"]:
         item = oldInv["itemData"][itemID]
-    else:
-        print(f"ERROR: ITEM_MOVE: Key not found: {itemID}\nERROR: Resending Inventory update back to the player")
-        # resending it, triggers a force-reopen of the Inventory (instantly)
-        dataset = {
-            "itemData": client.cData["itemData"],
-            "inv_grid": client.cData["inv_grid"]
-            }
-        asc_g_msg.sendMsg("player_gear_set", dataset, client.sData.con_gameServer)
-        # send Inventory back to the player and trigger the Inventory UI to be reopen, so it loads the new data
-        inv_handler.inv_send_toClient(oldInv, client.con_client)
-
 
     # exit if item couldn't be added
     if item is None:
+        print(f"ERROR: ITEM_MOVE: Key not found: {itemID}\nERROR: Resending Inventory update back to the player")
+        # resending it, triggers a force-reopen of the Inventory (force reopen/load Inventory)
+        inv_handler.inv_update_force(client=client, invID_old=invID_old, invID_new=invID_new)
         return
 
     # print(f"::::: item:\n{item}")
@@ -155,8 +148,8 @@ def item_move(client=None, args=()):
 
     # Check if the item used slots. In case of unequipping -> Ignore
     if len(slots_used_old) == 0 and not isUnEquip:
-        # ToDo: Send both Inventories back to the player, to update his UI (later)
         print("INVENTORY: Something was wrong with the old Item State - no blocked tiles found")
+        inv_handler.inv_update_force(client=client, invID_old=invID_old, invID_new=invID_new)
         return
 
     print(f"--------------------\n  EQUIP REQUEST: {isEquip}\nUNEQUIP REQUEST: {isUnEquip}\n--------------------")
@@ -172,8 +165,8 @@ def item_move(client=None, args=()):
 
     # check if there was enough space in the new Inventory. In case of Equip: Ignore
     if len(slots_used_new) == 0 and not isEquip:
-        # ToDo: Send both Inventories back to the player, to update his UI (later)
         print("INVENTORY: Item can NOT be added")
+        inv_handler.inv_update_force(client=client, invID_old=invID_old, invID_new=invID_new)
         return
     else:
         # and remove it from the old Inventory Grid, if its not coming from a Slot
@@ -365,3 +358,4 @@ def item_degrade(sData, user, itemType, *args):
               f"\nammoType - {ammoType}"
               f"\nshots - {shots}")
         # print(f"DEBUG: item_degrade - userdata: {sData.database.players[user]}")
+
