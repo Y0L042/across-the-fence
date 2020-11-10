ed59fa2fb04035b8c2833571fc25291f0f960831
Dscha <Eggi2105@googlemail.com>
2020-11-10 04:28:01 +0100

    Backend: cmdList: command adjusted: "itemMove" > "inv_itemMove"

--- packed/asc_files/cmdList.py
+++ packed/asc_files/cmdList.py
@@ -64,7 +64,7 @@ cmdList = {
 		'client': {
 				# ""Tag" send from Arma" : Function in the backend
 				# NOTE: Make sure, that you imported the related plugin/method/folder/however it's called (and added the files to the "__init.py__" )
-				"itemMove":         item_handler.item_move,
+				"inv_itemMove":     item_handler.item_move,
 				"inv_get_grid":     inv_handler.inv_grid_get,
 				"inv_get_items":    inv_handler.inv_items_get
 			},
